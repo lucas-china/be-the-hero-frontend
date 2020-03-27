19b46b276fac94ab04a5a6fed8c91fd3250bae84
Lucas <lucasbrito_9@hotmail.com>
2020-03-26 21:52:06 -0300

    Created routes with react router dom.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import './global.css';
 
-import Logon from './pages/logon'
+import Routes from './routes';
+
 
 function App() {
-  
-
   return (
     <div>
-      <Logon />
+      <Routes />
     </div>
   );
 }
